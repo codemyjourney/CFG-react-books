f625e8ac6c7894263cd4c42bd1b03cd123f59e78
codemyjourney <s.karlinska@hotmail.com>
2021-08-06 16:03:01 -0400

    connect frontend with backend

--- react-books-client/src/Root.js
+++ react-books-client/src/Root.js
@@ -1,6 +1,29 @@
 import React from "react";
 import withRoot from "./withRoot";
+import {Query} from 'react-apollo';
+import {gql} from "apollo-boost";
 
-const Root = () => <div>Root</div>;
+const Root = () => (
+<Query query={GET_TRACKS_Q}>
+{({ data, loading, error })=> {
+    if (loading) return <div>loading...</div>
+    if (error) return <div>error</div>
+
+    return <div>{JSON.stringify(data)}</div>
+}}
+
+</Query>
+);
+
+const GET_TRACKS_Q = gql`
+{
+    books {
+        id
+        title
+        author
+        description
+    }
+}
+`
 
 export default withRoot(Root);
