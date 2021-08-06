7a77ff04498d974675b78a5b1cd517cd50e7819d
codemyjourney <s.karlinska@hotmail.com>
2021-08-06 15:20:22 -0400

    add base frontend

---
+++ react-books-client/src/components/Book/DeleteBook.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import IconButton from "@material-ui/core/IconButton";
+// import TrashIcon from "@material-ui/icons/DeleteForeverOutlined";
+
+const DeleteBook = () => {
+  return <div>DeleteBook</div>;
+};
+
+export default DeleteBook;
